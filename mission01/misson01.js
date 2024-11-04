1772904e74336d9329ac6de2d3ed17333740221f
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2024-11-04 10:12:06 +0900

    선생님 과제풀이 추가

--- mission01/misson01.js
+++ mission01/misson01.js
@@ -31,6 +31,10 @@ function getValueAtObject(obj, key) {
 // const getNumberAtArray = (arr, index) => arr[index];
 
 function getNumberAtArray(arr, index) {
+  if (!Array.isArray(arr)) {
+    throw new Error("전달된 첫번째 인수의 데이터 타입은 배열 타입입니다. getNumberAtArray 함수의 첫 번째 인수는 배열 타입 이여야 합니다.");
+  }
+
   if (index >= 0 && index < arr.length) {
     return arr[index];
   } else {
@@ -45,3 +49,77 @@ console.log(getNumberAtArray(numbers, 4)); // 50
 console.log(getNumberAtArray(numbers, 0)); // 10
 console.log(getNumberAtArray(numbers, 5)); // Error!
 console.log(getNumberAtArray(numbers, -1)); // Error!
+
+console.log("-------------------------------------- 과제풀이");
+
+const object = {
+  name: "이우빈",
+  age: 30,
+};
+
+// 객체에 키를 입력했을 때 값이 나오길 기대
+// 함수는 매개변수를 유연하게 받을수록 재사용성이 증가
+
+function getValueAtObject(object, key) {
+  // if (!(Object.prototype.toString.call(object).slice(8, -1).toLowerCase() === "object")) {
+  //   console.error("getValueAtObject 함수의 첫 번째 인수는 객체 타입 이여야 합니다.");
+  // }
+
+  if (!(typeof object === "object")) {
+    throw new TypeError("getValueAtObject 함수의 첫 번째 인수는 객체 타입 이여야 합니다.");
+  }
+
+  if (typeof object !== "string") {
+    throw new TypeError("getValueAtObject 함수의 첫 번째 인수는 문자 타입 이여야 합니다.");
+  }
+
+  if (!(key in object)) {
+    throw new SyntaxError("해당 객체에는 key값이 존재하지 않습니다.");
+  }
+  return object[key];
+}
+
+getValueAtObject(object, "name"); // '이우빈'
+
+// 뭐 부터 시작하지?
+
+/**
+ * 1. 함수의 이름
+ * 2. 함수의 실행부 직접 작성 인자 작성
+ * 3. 함수의 로직
+ * 4. valdation
+ */
+
+// Object.prototype.toString();
+
+function _getValueAtObject(object, key) {
+  // entries는 배열로 만들어주는 메서드
+  const entries = Object.entries(object);
+
+  for (let keyValue of entries) {
+    const _key = keyValue[0];
+    const value = keyValue[1];
+
+    if (_key === key) {
+      return value;
+    }
+  }
+}
+
+console.log(_getValueAtObject(obj, "name"));
+
+const arr = ["정주연", "박혜미", "김영현"];
+
+function getNumberAtArray(array, index) {
+  if (!Array.isArray(array)) {
+    throw new Error(`전달된 첫 번째 인수의 데이터 타입은 ${typeof array} 타입 입니다. 해당 함수의 첫 번째 인수는 배열 타입 이어야 합니다.`);
+  }
+
+  if (index >= 0 && index < array.length) {
+    return array[index];
+  } else {
+    throw new Error("...");
+  }
+}
+
+console.log(getNumberAtArray(arr, 3));
